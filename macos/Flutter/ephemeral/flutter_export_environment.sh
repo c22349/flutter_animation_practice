364bc9438ca55fbf7742a95adcb7363327c5c51a
takezawa <s.take4173@gmail.com>
2025-01-07 23:39:26 +0900

    Add remaining Flutter files

--- macos/Flutter/ephemeral/flutter_export_environment.sh
+++ macos/Flutter/ephemeral/flutter_export_environment.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Users/shtk/Documents/flutter"
-export "FLUTTER_APPLICATION_PATH=/Users/shtk/Documents/code/flutter_animation_practice/flutter_animation_practice"
+export "FLUTTER_APPLICATION_PATH=/Users/shtk/Documents/code/flutter_animation_practice"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_BUILD_DIR=build"
 export "FLUTTER_BUILD_NAME=1.0.0"
