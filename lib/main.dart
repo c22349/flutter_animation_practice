c5a2be4328f98ae2a19b51abe66b6253f9aff5cc
takezawa <s.take4173@gmail.com>
2025-01-22 20:54:47 +0900

    Add image

--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blueAccent),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Animate Demo'),
     );
   }
 }
@@ -38,10 +38,10 @@ class MyHomePage extends StatefulWidget {
   // どのように見えるかに影響するフィールドを含みます。
 
   // このクラスは、ステートの設定です。このクラスは状態の設定です。
-  // 親ウィジェット（この場合は App ウィジェット）から提供された値（この場合はタイトル）を保持し、
+  // 親ウィジェット（この場合はAppウィジェット）から提供された値（この場合はタイトル）を保持し、
   // App ウィジェットのビルドメソッドで使用されます。
-  // ステートのビルドメソッドで使用されます。Widgetサブクラスのフィールドは
-  // 常に「final」とマークされます。
+  // ステートのビルドメソッドで使用されます。Widgetサブクラスのフィールドは常に「final」とマークされます。
+
   final String title;
 
   @override
@@ -49,19 +49,10 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // このsetStateの呼び出しは、Flutterフレームワークに、このStateで何かが変更されたことを // 伝える。
-      // これにより、以下のビルドメソッドを再実行する。
-      // これにより、以下のビルドメソッドが再実行され、更新された値がディスプレイに反映されます。
-      // もしsetState()を呼び出さずに__counterを変更した場合、ビルドメソッドは再度呼び出されない。
-      // そのため、何も起こらないように見える。
-      _counter++;
-    });
-  }
-
+  // このsetStateの呼び出しは、Flutterフレームワークに、このStateで何かが変更されたことを伝える。
+  // これにより、以下のビルドメソッドが再実行され、更新された値がディスプレイに反映されます。
+  // もしsetState()を呼び出さずに__counterを変更した場合、ビルドメソッドは再度呼び出されない。
+  // そのため、何も起こらないように見える。
   @override
   Widget build(BuildContext context) {
     // このメソッドは、setStateが呼ばれるたびに再実行される。
@@ -72,37 +63,25 @@ class _MyHomePageState extends State<MyHomePage> {
     // ウィジェットのインスタンスを個別に変更するのではなく、更新が必要なものを再構築するだけでよい。
     return Scaffold(
       appBar: AppBar(
-        // ここの色を特定の色に変更してみてください。
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         // ここでは、App.buildメソッドで作成されたMyHomePageオブジェクトから値を取り出し、それを使ってappbarのタイトルを設定する。
         // App.buildメソッドによって作成されたMyHomePageオブジェクトから値を取得し、それを使用してappbarのタイトルを設定します。
         title: Text(widget.title),
       ),
       body: Center(
-        // センターはレイアウトウィジェットです。子ウィジェットを1つ取り配置します。
+        // カラムもレイアウト・ウィジェットです。子ウィジェットのリストを受け取り縦に並べます。デフォルトでは親と同じ高さになるようにします。
+        // カラムは、それ自身のサイズと // 子の位置を制御するためのさまざまなプロパティを持っています。
+        // どのように子プロパティを配置するかを制御します。ここでは、mainAxisAlignment を使用して、
+        // 子を垂直に中央に配置します。主軸は垂直軸です。
+        //
+        // これを試してください： デバッグペイント」を起動します（IDEで 「Toggle Debug Paint 」アクションを選択するか「p 」キーを押します）。
+        // IDE で 「Toggle Debug Paint」 アクションを選択するか、コンソールで 「p」 を押します）。
+        // 各ウィジェットのワイヤーフレームを見ることができます。
         child: Column(
-          // カラムもレイアウト・ウィジェットです。子ウィジェットのリストを受け取り縦に並べます。デフォルトでは親と同じ高さになるようにします。
-          // カラムは、それ自身のサイズと // 子の位置を制御するためのさまざまなプロパティを持っています。
-          // どのように子プロパティを配置するかを制御します。ここでは、mainAxisAlignment を使用して、
-          // 子を垂直に中央に配置します。主軸は垂直軸です。
-          //
-          // これを試してください： デバッグペイント」を起動します（IDEで 「Toggle Debug Paint 」アクションを選択するか「p 」キーを押します）。
-          // IDE で 「Toggle Debug Paint」 アクションを選択するか、コンソールで 「p」 を押します）。
-          // 各ウィジェットのワイヤーフレームを見ることができます。
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+          children: [Image.asset('images/walk.png')],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // この末尾のカンマは、ビルド・メソッドの自動書式設定に便利である。
     );
   }
 }
